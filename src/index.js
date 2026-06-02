fc600799e053f10d19f24688e0f563d10a90cc94
portiaportia <portiaplante@gmail.com>
2026-06-02 11:57:47 -0400

    testing routing

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const App = () => {
         <Routes>
           <Route path = "/" element = {<Layout />}>
             <Route index element={<Home />} />
-            <Route path="/education" element={<Education />} />
+            <Route path="education" element={<Education />} />
             <Route
               path="education/:slug"
               element={<Article />}
